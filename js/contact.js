9882150172d1d634bc6ed6169eb3310d9d929f70
dhyanuwebsolution@gmail.com <dhyanuwebsolution@gmail.com>
2019-02-19 17:43:55 +0000

    test

--- js/contact.js
+++ js/contact.js
@@ -19,22 +19,16 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url:"https://functiondhyanuwebsolution.azurewebsites.net/api/HttpTrigger1?code=ss1gLXmvaQDeqvbElPWa8WgGOT0vpttleqsynnAeCwj1g4UYfKbxmg==", // "././mail/contact_me.php",
-                type: "POST",
-                data: {
+            url:"https://functiondhyanuwebsolution.azurewebsites.net/api/HttpTrigger1?code=ss1gLXmvaQDeqvbElPWa8WgGOT0vpttleqsynnAeCwj1g4UYfKbxmg==",
+            type: "POST",
+            data: {
                      "name": "Azure"
                   },
-                //{
-                 //   name: name,
-                 //   phone: phone,
-                 //   email: email,
-                 //   message: message
-                //},
-                cache: false,
-                success: function (result) {
-                    console.log(result);
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
+            contentType: "application/json; charset=UTF-8",
+            dataType: "json",
+            success: function (results) {
+                console.log(result);
+                $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
@@ -43,23 +37,22 @@ $(function () {
                         .append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
-                },
-                error: function () {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                 $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
-                },
-                complete: function () {
+            },
+            complete: function () {
                     setTimeout(function () {
                         $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
                     }, 1000);
                 }
-            });
+          });
         },
         filter: function () {
             return $(this).is(":visible");
